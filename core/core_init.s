c1b9ea24673cc9f4f26ae7656c489444d4fbc469
Federico Borello <fborello@frba.utn.edu.ar>
2024-05-07 22:50:12 -0300

    fix: Fix makefile and logs

--- core/core_init.s
+++ core/core_init.s
@@ -4,7 +4,10 @@
 .section .text
 _start: 
     ldr sp, =__sp
-    mov r0, #37
-    mov r1, #3
+    mov r4, #0x20
+    mov r5, #0x10
+    add r6, r5, r4
+    mov r0, #0x20
+    mov r1, #0x10
     bl sum
     b .
