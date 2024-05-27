0e609965900f07a97d8937d114e1c4491c824436
Federico Borello <fborello@frba.utn.edu.ar>
2024-05-26 21:49:01 -0300

    feat(vector_table): copy to the start of memory

--- core/startup.s
+++ core/startup.s
@@ -3,6 +3,12 @@
 
 .section .text._start
 _start: 
+    // store _vector_table at address 0x0000_0000
+    ldr     r0, =_vector_table
+    mov     r1, #0x0
+    ldmia   r0!, {r2-r8}
+    stmia   r1!, {r2-r8}
+
     ldr sp, =__sp
     mov r0, #0x20
     mov r1, #0x10
