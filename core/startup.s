d692df4231a77fefe911ea7966d7ba4fa81a773f
Federico Borello <fborello@frba.utn.edu.ar>
2024-05-27 01:09:12 -0300

    refactor(vector_table): copy vector_table inside reset_handler

--- core/startup.s
+++ core/startup.s
@@ -2,12 +2,6 @@
 
 .section .text._start
 _start: 
-    // store _vector_table at address 0x0000_0000
-    ldr     r0, =_vector_table
-    mov     r1, #0x0
-    ldmia   r0!, {r2-r8}
-    stmia   r1!, {r2-r8}
-
     mov r0, #0x20
     mov r1, #0x10
     add r2, r1, r0 
